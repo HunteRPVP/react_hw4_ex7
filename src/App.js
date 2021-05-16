900c13fc5e3d7bc5f5fc90545fc6ed3b0d0485b1
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-05-16 18:58:02 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: true,
+      name: 'Иван'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {this.state.show ? 'Привет, ' + this.state.name + '!' : 'Пока, ' + this.state.name + '!'}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
